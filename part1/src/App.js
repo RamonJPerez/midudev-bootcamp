667012d061d917b045542596e16f51249abab4e8
Ramon <104288305+RamonJPerez@users.noreply.github.com>
2022-08-05 13:59:01 -0400

    midudev

---
+++ part1/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<h1>Titulo de la App</h1>
+<strong>Estamos trabajando en ello</strong>
+    </div>
+  );
+}
+
+export default App;
